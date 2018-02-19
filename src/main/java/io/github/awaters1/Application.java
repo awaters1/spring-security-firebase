fb3322471c0c213a23a29b2bc0ecdec2e5c30c52
megalucio <megalucio@localhost>
2018-02-19 09:11:05 -0500

    Upgrading to spring 1.5.10 and firebase 5.9.0

--- src/main/java/io/github/awaters1/Application.java
+++ src/main/java/io/github/awaters1/Application.java
@@ -1,5 +1,6 @@
 package io.github.awaters1;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,38 +20,36 @@ import com.google.firebase.auth.FirebaseAuth;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-	
+		SpringApplication.run(Application.class, args);
+	}
+
 	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 	@Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // you USUALLY want this
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedHeader("x-firebase-auth");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-	
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true); // you USUALLY want this
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedHeader("x-firebase-auth");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
 	@Bean
-	public FirebaseAuth firebaseAuth() {
+	public FirebaseAuth firebaseAuth() throws IOException {
 		// TODO: Fill in correct firebase auth
-		InputStream in =
-	            Application.class.getResourceAsStream("/firebase-adminsdk.json");
-		FirebaseOptions options = new FirebaseOptions.Builder()
-				  .setServiceAccount(in)
-				  .setDatabaseUrl("https://mydatabaseurl.firebaseio.com/")
-				  .build();
+		InputStream in = Application.class.getResourceAsStream("/firebase-adminsdk.json");
+
+		FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(in))
+				.setDatabaseUrl("https://mydatabaseurl.firebaseio.com/").build();
 
 		FirebaseApp.initializeApp(options);
 		return FirebaseAuth.getInstance();
--- src/main/java/io/github/awaters1/security/FirebaseAuthenticationProvider.java
+++ src/main/java/io/github/awaters1/security/FirebaseAuthenticationProvider.java
@@ -1,6 +1,5 @@
 package io.github.awaters1.security;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +10,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 import org.springframework.stereotype.Component;
 
+import com.google.api.core.ApiFuture;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseToken;
-import com.google.firebase.tasks.OnSuccessListener;
 
 import io.github.awaters1.security.model.FirebaseAuthenticationToken;
 import io.github.awaters1.security.model.FirebaseUserDetails;
@@ -21,28 +20,30 @@ import io.github.awaters1.security.model.FirebaseUserDetails;
 @Component
 public class FirebaseAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
 
-    @Autowired
-    private FirebaseAuth firebaseAuth;
+	@Autowired
+	private FirebaseAuth firebaseAuth;
 
-    @Override
-    public boolean supports(Class<?> authentication) {
-        return (FirebaseAuthenticationToken.class.isAssignableFrom(authentication));
-    }
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return (FirebaseAuthenticationToken.class.isAssignableFrom(authentication));
+	}
 
-    @Override
-    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
-    }
+	@Override
+	protected void additionalAuthenticationChecks(UserDetails userDetails,
+			UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+	}
 
-    @Override
-    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
-        final FirebaseAuthenticationToken authenticationToken = (FirebaseAuthenticationToken) authentication;
-        final CompletableFuture<FirebaseToken> future = new CompletableFuture<>();
-        firebaseAuth.verifyIdToken(authenticationToken.getToken()).addOnSuccessListener(future::complete);
+	@Override
+	protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication)
+			throws AuthenticationException {
+		final FirebaseAuthenticationToken authenticationToken = (FirebaseAuthenticationToken) authentication;
+
+		ApiFuture<FirebaseToken> task = firebaseAuth.verifyIdTokenAsync(authenticationToken.getToken());
 		try {
-			final FirebaseToken token = future.get();
+			FirebaseToken token = task.get();
 			return new FirebaseUserDetails(token.getEmail(), token.getUid());
 		} catch (InterruptedException | ExecutionException e) {
 			throw new SessionAuthenticationException(e.getMessage());
 		}
-    }
+	}
 }
